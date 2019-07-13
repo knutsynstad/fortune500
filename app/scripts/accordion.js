da07c2e0739e6afaed7a0e22bcb5b1e9c10a913c
Knut Synstad <knut@synstad.net>
2019-07-13 16:37:11 -0700

    initial commit

--- app/scripts/accordion.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => {
-  document.querySelectorAll('ol .icon')
-    .forEach((row) => {
-      row.addEventListener('click', () => {
-        row.parentNode.parentNode.classList.toggle('open')
-      }, false)
-    })
-}
